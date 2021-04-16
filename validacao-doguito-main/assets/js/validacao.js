296fd7358523d1dd22fa48f3126536a919e84699
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-04-16 08:52:20 -0300

    Lógica de validação de data caso seje >18 e criação do arquivo validacao.js

---
+++ validacao-doguito-main/assets/js/validacao.js
@@ -0,0 +1,24 @@
+const dataNascimento = document.querySelector('#nascimento')         //Acessando o input de data
+
+dataNascimento.addEventListener('blur', (evento) => {                // (blur = quando perde foco do campo)
+    validaDataNascimento(evento.target)
+})
+
+function validaDataNascimento(input) {
+    const dataRecebida  = new Date(input.value)
+    let mensagem = ''                                                  // Passando a string p/ >18
+
+    if(!maiorQue18(dataRecebida)) {                                    // ! == contrário | verificação
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'   
+    }
+
+    input.setCustomValidity(mensagem)                                 //  Passando string p/ < 18
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date()                                     // new Date() sem parâmetro trará a data atual
+
+    const dataMais18 = new Date(data.getFullYear() + 18, data.getUTCMonth(), date.getUTCdate())       // Formato que o javaScript espera
+    
+    return dataMais18 <= dataAtual
+}
