80716ba4ac1d8da256653159ec55331149ad6ba5
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-04-17 18:36:45 -0300

    Criando arquivo app.js e tornando genérica a função de validação de input

--- validacao-doguito-main/assets/js/validacao.js
+++ validacao-doguito-main/assets/js/validacao.js
@@ -1,3 +1,15 @@
+ export function valida() {                                                 //Função que valida um tipo de input
+    const tipoDeInput = input.dataset.tipoDeInput                            // Selecionando o input
+    
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)                                      // Validando
+    }                        
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
 const dataNascimento = document.querySelector('#nascimento')         //Acessando o input de data
 
 dataNascimento.addEventListener('blur', (evento) => {                // (blur = quando perde foco do campo)
@@ -12,7 +24,7 @@ function validaDataNascimento(input) {
         mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'   
     }
 
-    input.setCustomValidity(mensagem)                                 //  Passando string p/ < 18
+    input.setCustomValidity(mensagem)                                 //  Defini uma mensagem de erro customizada (com let mensagem)
 }
 
 function maiorQue18(data) {
