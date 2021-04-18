7b0596e3fd2262b01fb9fec97555ac3d7a5ae509
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-04-18 07:56:57 -0300

    Inserindo mensagens de erro personalizadas no formulário

--- validacao-doguito-main/assets/js/validacao.js
+++ validacao-doguito-main/assets/js/validacao.js
@@ -1,13 +1,36 @@
- export function valida() {                                                 //Função que valida um tipo de input
-    const tipoDeInput = input.dataset.tipoDeInput                            // Selecionando o input
+ export function valida(input) {                                                 //Função que valida um tipo de input
+    const tipoDeInput = input.dataset.tipo                          // Selecionando o input
     
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)                                      // Validando
-    }                        
+    }   
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+    }                  
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input)
+}
+
+const mensagensDeErro =  {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo email não pode estar vazio.',
+        typeMissMatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio.',
+        patterMisMatch: 'Senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
 }
 
 const dataNascimento = document.querySelector('#nascimento')         //Acessando o input de data
